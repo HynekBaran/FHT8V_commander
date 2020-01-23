5ca93ac71ad20a6689586df6b3a8d997f45b5ee4
Jiri Tlusty @macbook <jiri.tlusty@atlas.cz>
2020-01-23 12:44:46 +0100

    dude-it.sh : service fhem stopped before flashing

--- dude-it.sh
+++ dude-it.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 # dude-it.sh simple script to avrdude directly no ttyUSB0 (DTR must be connected), 
 # use Sketch : Export compiled Binary  in Arduino IDE to generate fhtavr.ino.eightanaloginputs.hex
+fuser /dev/ttyUSB0
+read -rsn1 -p"Press enter to stop fhem service";echo
+service fhem stop
+fuser /dev/ttyUSB0
+read -rsn1 -p"Press enter to reflash /dev/ttyUSB0";echo
 avrdude -v -patmega328p -carduino -P/dev/ttyUSB0 -b57600 -D -Uflash:w:fhtavr.ino.eightanaloginputs.hex:i 
